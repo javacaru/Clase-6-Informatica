f11d6a00c9fb109b37a3d78519891aa583ddbdbe
javacaru <janna.castaneda@udea.edu.co>
2025-03-25 17:48:37 -0500

    PRIMEROS CODIGOS 💕

---
+++ clase informatica.py
@@ -0,0 +1,9 @@
+a=int(input("ingresa un numero"))
+b=int(input("ingresa otro numero"))
+c=0
+resultado=0
+while c< a:
+  resultado += b
+  print(resultado)
+  c+=1
+print(f" El esultado final es {resultado}")
